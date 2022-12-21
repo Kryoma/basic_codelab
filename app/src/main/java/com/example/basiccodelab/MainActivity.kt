ba36bda6e88b76e311bc4ed1f45ac35ede03594d
Kryoma <ryoma.kuboshi@gmail.com>
2022-12-22 08:45:59 +0900

    拡張画面にテキストを追加。animateContentSize実装

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.basiccodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,11 +13,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccodelab.ui.theme.BasicCodelabTheme
@@ -67,22 +75,47 @@ fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                //アニメーション作成プロセスを自動化する。
+                .animateContentSize()
+        ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    // クラッシュしないようにパディングに負数が入らないようにする。
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                // クラッシュしないようにパディングに負数が入らないようにする。
+                //RpwにanimateContentSize修飾子を適用させて代用
+//                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
+                if (expanded) Text(text = "お試しテキスト。")
             }
-            Button(
-                onClick = { expanded = !expanded },
-                border = BorderStroke(width = 1.dp, color = Color.White)
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
+            IconButton(
+                onClick = { expanded = !expanded }) {
+                Icon(
+                    ImageVector =
+                    if (expanded) Icons.Filled.ExpandLess
+                    else Icons.Filled.ExpandMore,
+                    contentDescription =
+                    if (expanded) {
+                        stringResource(id = (R.string.show_less))
+                    } else {
+                        stringResource(id = R.string.show_more)
+                    }
+                )
+
             }
+//            Button(
+//                onClick = { expanded = !expanded },
+//                border = BorderStroke(width = 1.dp, color = Color.White)
+//            ) {
+//                Text(
+//                    if (expanded) stringResource(R.string.show_less)
+//                    else stringResource(R.string.show_more)
+//                )
+//            }
         }
 
     }
@@ -126,7 +159,12 @@ fun OnBoardingScreen(
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Composable
 fun DefaultPreview() {
     BasicCodelabTheme {
--- app/src/main/java/com/example/basiccodelab/ui/theme/Theme.kt
+++ app/src/main/java/com/example/basiccodelab/ui/theme/Theme.kt
@@ -5,11 +5,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Navy,
+    onSurface = Navy,
+    surface = Blue,
+    onPrimary = Chartreuse
 )
 
 private val LightColorPalette = lightColors(
@@ -41,4 +43,5 @@ fun BasicCodelabTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Comp
         shapes = Shapes,
         content = content
     )
+
 }
